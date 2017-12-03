a103b5ee2b2f22a877e2d4615ebc09cc7da54a3d
HiromuFuji <fuji.hiromu@gmail.com>
2017-12-03 20:42:18 +0900

    Initial Commit

---
+++ pro2_wifi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pro2_wifi
+//
+//  Created by Fuji Hiromu on 2017/12/03.
+//  Copyright © 2017年 藤　大夢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
